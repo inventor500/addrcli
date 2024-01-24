0e183076e7a31fa5b8dd2f4bbc83b7db95a88f27
inventor500 <isaacganoung@gmail.com>
2024-01-23 21:04:02 -0600

    Colors now only print when connected to a terminal

--- types.go
+++ types.go
@@ -17,6 +17,20 @@ import (
 	"strings"
 )
 
+func Sprintf(format string, colored bool, args ...interface{}) string {
+	var str string
+	if colored {
+		str = fmt.Sprintf("\033[1m%s\033[0m", format)
+	} else {
+		str = format
+	}
+	return fmt.Sprintf(str, args...)
+}
+
+type Result interface {
+	StringFormatted(bool) string
+}
+
 type Address struct {
 	Company  string `json:"companyName"`
 	Address1 string `json:"addressLine1"`
@@ -31,32 +45,32 @@ type Address struct {
 func (address Address) String() string {
 	var result strings.Builder
 	if address.Company != "" {
-		result.WriteString(fmt.Sprintf("%s, ", address.Company))
+		result.WriteString(address.Company)
 	}
-	result.WriteString(fmt.Sprintf("%s, ", address.Address1))
+	result.WriteString(address.Address1)
 	if address.Address2 != "" {
-		result.WriteString(fmt.Sprintf("%s, ", address.Address2))
+		result.WriteString(address.Address2)
 	}
 	result.WriteString(fmt.Sprintf("%s, %s %s", address.City, address.State, address.Zip5))
 	if address.Zip4 != "" {
-		result.WriteString(fmt.Sprintf("-%s", address.Zip4))
+		result.WriteString("-" + address.Zip4)
 	}
 	return result.String()
 }
 
 // The same as String, but with new lines
-func (address Address) StringFormatted() string {
+func (address Address) StringWithLines() string {
 	var result strings.Builder
 	if address.Company != "" {
-		result.WriteString(fmt.Sprintf("%s\n", address.Company))
+		result.WriteString(fmt.Sprintln(address.Company))
 	}
-	result.WriteString(fmt.Sprintf("%s\n", address.Address1))
+	result.WriteString(fmt.Sprintln(address.Address1))
 	if address.Address2 != "" {
-		result.WriteString(fmt.Sprintf("%s\n", address.Address2))
+		result.WriteString(fmt.Sprintln(address.Address2))
 	}
 	result.WriteString(fmt.Sprintf("%s, %s %s", address.City, address.State, address.Zip5))
 	if address.Zip4 != "" {
-		result.WriteString(fmt.Sprintf("-%s\n", address.Zip4))
+		result.WriteString("-" + address.Zip4)
 	}
 	return result.String()
 }
@@ -68,6 +82,19 @@ type AddressQueryResult struct {
 
 func (result AddressQueryResult) String() string {
 	return fmt.Sprintf("%s: %s", result.Status, result.Addresses)
+}
+
+func (result AddressQueryResult) StringFormatted(colored bool) string {
+	if result.Status != "SUCCESS" || len(result.Addresses) == 0 {
+		return Sprintf("No results", colored)
+	}
+	var resultString strings.Builder
+	resultString.WriteString(Sprintf("Collected Addresses\n", colored))
+	resultString.WriteString(result.Addresses[0].StringWithLines())
+	for i := 1; i < len(result.Addresses); i++ {
+		resultString.WriteString("\n\n" + result.Addresses[i].StringWithLines())
+	}
+	return resultString.String()
 }
 
 // Athens, GR
@@ -94,6 +121,31 @@ func (result CityQueryResult) String() string {
 	return fmt.Sprintf("%s: %s, %s %s (%d alternatives, %d non-accepts)", result.Status, result.City, result.State, result.Zip, len(result.OtherCities), len(result.NonAccept))
 }
 
+func (res CityQueryResult) StringFormatted(colored bool) string {
+	if res.Status != "SUCCESS" {
+		return Sprintf("No results\n", colored)
+	}
+	var resultString strings.Builder
+	resultString.WriteString(Sprintf("%s\n", colored, res.Zip))
+	resultString.WriteString(fmt.Sprintf("%s, %s %s", res.City, res.State, res.Zip))
+	if res.Caveat != "" {
+		resultString.WriteString(fmt.Sprintf("(%s)", res.Caveat))
+	}
+	if len(res.OtherCities) > 0 {
+		resultString.WriteString(Sprintf("\n\n%d Other Cities", colored, len(res.OtherCities)))
+		for i := 0; i < len(res.OtherCities); i++ {
+			resultString.WriteString("\n" + res.OtherCities[i].String())
+		}
+	}
+	if len(res.NonAccept) > 0 {
+		resultString.WriteString(Sprintf("\n\n%d Not Accepted Cities", colored, len(res.NonAccept)))
+		for i := 0; i < len(res.NonAccept); i++ {
+			resultString.WriteString("\n" + res.NonAccept[i].String())
+		}
+	}
+	return resultString.String()
+}
+
 type Zip struct {
 	Zip5   string `json:"zip5"`
 	Caveat string `json:"recordType"`
@@ -115,4 +167,16 @@ type ZipQueryResult struct {
 
 func (result ZipQueryResult) String() string {
 	return fmt.Sprintf("%s, %s: %s", result.City, result.State, result.Zips)
+}
+
+func (result ZipQueryResult) StringFormatted(colored bool) string {
+	if result.Status != "SUCCESS" || len(result.Zips) == 0 {
+		return Sprintf("No results", colored)
+	}
+	var returnValue strings.Builder
+	returnValue.WriteString(Sprintf("%s, %s", colored, result.City, result.State))
+	for i := 0; i < len(result.Zips); i++ {
+		returnValue.WriteString("\n" + result.Zips[i].String())
+	}
+	return returnValue.String()
 }
